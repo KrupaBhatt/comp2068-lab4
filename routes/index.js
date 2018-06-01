4788f5a983307a5d29dbe1193c71cea78ceb26b8
Krupa Bhatt <Krupa.Bhatt@MyGeorgian.ca>
2018-06-01 10:54:16 -0400

    last commit

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ const indexController = require('../controllers/indexController');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/index', indexController.index);
+router.get('/', indexController.index);
 router.get('/krupa', indexController.krupa);
 router.get('/jitendra', indexController.jitendra);
 router.get('/parul', indexController.parul);
 router.get('/keisi', indexController.keisi);
-router.get('/', (req, res) => res.send("Oops!"));
+
 module.exports = router;
