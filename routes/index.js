040cd82c6a5d2ca35fdc421e4daf838a29a4bf5c
Krupa Bhatt <Krupa.Bhatt@MyGeorgian.ca>
2018-06-01 10:13:26 -0400

    committed changes

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,5 @@ router.get('/krupa', indexController.krupa);
 router.get('/jitendra', indexController.jitendra);
 router.get('/parul', indexController.parul);
 router.get('/keisi', indexController.keisi);
-router.get('/', (req, res) => res.send("Oops. I didn't code this route"));
+router.get('/', (req, res) => res.send("Oops!"));
 module.exports = router;
